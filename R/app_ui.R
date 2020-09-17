9875f9806e362a6285398f47fb2cb68804f4c1c6
Octavio Morante Palacios <octaviompa@gmail.com>
2020-09-17 16:47:09 +0200

    version changed

--- R/app_ui.R
+++ R/app_ui.R
@@ -413,7 +413,7 @@ app_ui <- function(request) {
       img(src = "images/logo.png",
           width = 150), 
       h1("shiny\u00C9PICo") ,
-      h3("1.0.0-dev"),
+      h3("1.0.0"),
       br(),
       h4(
         tags$a(href = "https://www.gnu.org/licenses/agpl-3.0.html", "GNU Affero GPLv3 License", target="_blank")
