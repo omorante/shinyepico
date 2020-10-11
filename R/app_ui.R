a62d75961c9979de60544c7106e0d8c1e6485310
Octavio Morante-Palacios <octaviompa@gmail.com>
2020-10-11 16:41:44 +0200

    version bump 1.1.0

--- R/app_ui.R
+++ R/app_ui.R
@@ -476,7 +476,7 @@ app_ui <- function(request) {
       img(src = "images/logo.png",
           width = 150), 
       h1("shiny\u00C9PICo") ,
-      h3("1.0.0"),
+      h3("1.1.0"),
       br(),
       h4(
         tags$a(href = "https://www.gnu.org/licenses/agpl-3.0.html", "GNU Affero GPLv3 License", target="_blank")
